0b5b851a253980b3c7964afeb3416d077b251b00
arjun singh <arjunvkas@gmail.com>
2026-05-13 12:57:24 +0530

    [Arjun] Refactor.code refactored

--- SkyBookerPlatform/SkyBooker.ApiGateway/Program.cs
+++ SkyBookerPlatform/SkyBooker.ApiGateway/Program.cs
@@ -13,16 +13,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-                "http://localhost:3000",
-                "http://localhost:3001",
-                "http://localhost:3002",
-                "http://localhost:3003",
-                "http://localhost:5173"
-              )
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
